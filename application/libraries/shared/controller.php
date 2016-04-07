153e736dc87a6e317dd8cf7c365544847738df9a
Faizan Ayubi <indianayubi@gmail.com>
2016-04-08 03:48:01 +0530

    Flow Email

--- application/libraries/shared/controller.php
+++ application/libraries/shared/controller.php
@@ -98,6 +98,46 @@ class Controller extends \Framework\Controller {
         return FALSE;
     }
 
+    /**
+     * The Main Method to return Mailgun Instance
+     * 
+     * @return \Mailgun\Mailgun Instance of Mailgun
+     */
+    protected function mailgun() {
+        $configuration = Registry::get("configuration");
+        $parsed = $configuration->parse("configuration/mail");
+
+        if (!empty($parsed->mail->mailgun) && !empty($parsed->mail->mailgun->key)) {
+            $mg = new \Mailgun\Mailgun($parsed->mail->mailgun->key);
+            return $mg;
+        }
+    }
+    
+    protected function getBody($options) {
+        $template = $options["template"];
+        $view = new \Framework\View(array(
+            "file" => APP_PATH . "/application/views/layouts/email/{$template}.html"
+        ));
+        foreach ($options as $key => $value) {
+            $view->set($key, $value);
+        }
+
+        return $view->render();
+    }
+    
+    protected function notify($options) {
+        $body = $this->getBody($options);
+        $emails = isset($options["email"]) ? array($options["email"]) : array($options["user"]->email);
+        $mailgun = $this->mailgun();
+        $mailgun->sendMessage("cloudstuff.tech",array(
+            'from'    => 'Hemant Mann <hemant@cloudstuff.tech>',
+            'to'      => $emails,
+            'subject' => $options["subject"],
+            'text'    => $body
+        ));
+        $this->log(implode(",", $emails));
+    }
+
     public function __construct($options = array()) {
         parent::__construct($options);
 
